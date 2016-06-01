eda2b101df204a89eba33aa8808ec0496dceaae6
Vinicius Isola <viniciusisola@gmail.com>
2016-05-31 20:27:23 -0400

    Make input element more flexible

--- src/main/js/components/bootstrap/InputElement.js
+++ src/main/js/components/bootstrap/InputElement.js
@@ -11,26 +11,63 @@ export default class InputElement extends React.Component {
     this.setState({value: nextProps.value || ''});
   }
 
+  getValue () {
+    if (this.state.value == '') {
+      return null;
+    }
+    return this.state.value;
+  }
+
+  isToggleButton() {
+    let {type} = this.props;
+    return type == 'checkbox' || type == 'radio';
+  }
+
   handleChange (e) {
-    this.setState({value: e.target.value});
+    let value = this.isToggleButton() ? e.target.checked : e.target.value;
+    this.setState({value});
+    if (this.props.onChange) {
+      this.props.onChange(value, e);
+    }
   }
 
   render () {
-    let {propsClassName, ...props} = this.props;
+    if (this.isToggleButton()) {
+      return this.renderCheckboxOrRadio();
+    } else {
+      return this.renderOtherTypes();
+    }
+  }
+
+  renderCheckboxOrRadio() {
+    return <div className="form-group">
+      <label for={this.props.name}>
+        {this.renderInput(false)} {this.props.label}
+      </label>
+    </div>;
+  }
+
+  renderInput(formControl = true) {
+    let {className, type, ...props} = this.props;
+    type = type || 'text';
 
     let classes = {
-      "form-control": true
+      "form-control": formControl
     };
 
+    return <input
+      {...props}
+      type={type}
+      className={classNames(classes, className)}
+      onChange={this.handleChange.bind(this)}
+      value={this.state.value || ''}
+    />;
+  }
+
+  renderOtherTypes() {
     return <div className="form-group">
       <label for={this.props.name}>{this.props.label}</label>
-      <input
-        type="text"
-        {...props}
-        className={classNames(classes, propsClassName)}
-        onChange={this.handleChange.bind(this)}
-        value={this.state.value || ''}
-      />
+      {this.renderInput()}
     </div>;
   }
 
