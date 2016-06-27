9b9e0a61c2ec8da3dd64ff0e4e8511986566d733
Vinicius Isola <viniciusisola@gmail.com>
2016-06-26 20:55:01 -0400

    Split FormGroup logic into its own component

---
+++ src/main/js/components/bootstrap/FormGroup.js
@@ -0,0 +1,31 @@
+import classNames from 'classnames';
+import React from 'react';
+
+export default class FormGroup extends React.Component {
+
+  render () {
+    if (this.props.shouldRenderLabelBefore) {
+      return this.renderLabelBeforeChildren();
+    } else {
+      return this.renderChildrenInsideLabel();
+    }
+  }
+
+  renderChildrenInsideLabel() {
+    return <div className="form-group">
+      <label for={this.props.name}>
+        {this.props.children}
+        &nbsp;
+        {this.props.label}
+      </label>
+    </div>;
+  }
+
+  renderLabelBeforeChildren() {
+    return <div className="form-group">
+      <label for={this.props.name}>{this.props.label}</label>
+      {this.props.children}
+    </div>;
+  }
+
+}
